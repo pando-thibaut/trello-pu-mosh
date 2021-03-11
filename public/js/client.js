47959feb82038d1782da694120ae4411ebda0dfe
thibaut-pando <thibaut@pando-studio.com>
2021-03-11 17:18:07 +0100

    Adding estimate cardBoard

--- public/js/client.js
+++ public/js/client.js
@@ -345,7 +345,16 @@ TrelloPowerUp.initialize({
       text: 'Just a URL',
       url: 'https://developers.trello.com',
       target: 'Trello Developer Site' // optional target for above url
-    }];
+    }, {
+        icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+        text: 'Estimate Size',
+        callback: function(t){
+          return t.popup({
+            title: "Estimation",
+            url: 'estimate.html'
+          });
+        }
+      }];
   },
   'card-detail-badges': function(t, options) {
     return getBadges(t);
