32acc21cbccf2a09148c402be217d20a2bd1be0f
pando-thibaut <thibaut@pando-studio.com>
2021-03-16 14:48:57 +0100

    Adding auth functions

--- public/js/client.js
+++ public/js/client.js
@@ -375,20 +375,17 @@ TrelloPowerUp.initialize({
   'authorization-status': function(t, options){
     // Return a promise that resolves to an object with a boolean property 'authorized' of true or false
     // The boolean value determines whether your Power-Up considers the user to be authorized or not.
-    
+
     // When the value is false, Trello will show the user an "Authorize Account" options when
     // they click on the Power-Up's gear icon in the settings. The 'show-authorization' capability
     // below determines what should happen when the user clicks "Authorize Account"
-    
+
     // For instance, if your Power-Up requires a token to be set for the member you could do the following:
-    return t.get('member', 'private', 'token')
+    return t.get('member', 'private', 'authToken')
     // Or if you needed to set/get a non-Trello secret token, like an oauth token, you could
     // use t.storeSecret('key', 'value') and t.loadSecret('key')
-    .then(function(token){
-      if(token){
-        return { authorized: true };
-      }
-      return { authorized: false };
+    .then(function(authToken){
+      return { authorized: authToken != null }
     });
     // You can also return the object synchronously if you know the answer synchronously.
   },
